0ede862283114ef822379561bfb47504436daf89
Владимир Перетятько <djbob2000@gmail.com>
2023-01-05 00:27:26 +0200

    add proptypes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ContactItem from '../ContactItem/ContactItem';
 // import css from './ContactList.module.css';
 
@@ -15,5 +16,16 @@ const ContactList = ({ contacts, onDelete }) => (
     ))}
   </ul>
 );
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default ContactList;
